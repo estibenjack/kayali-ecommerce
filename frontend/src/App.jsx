0fafae131ad654732c97507ff48d37b87b39eec8
Steven <156148848+estibenjack@users.noreply.github.com>
2024-12-30 01:24:49 +0000

    Add header menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
+import HeaderMenu from './components/HeaderMenu/HeaderMenu';
 import Hero from './components/Hero/Hero';
 import bannerMessages from './data-folder/banner-messages';
 
@@ -12,6 +13,7 @@ function App() {
    <div>
     <Banner messages={bannerMessages} interval={5000} />
     <Navbar />
+    <HeaderMenu />
     <Hero />
    </div>
   )
---
+++ frontend/src/components/HeaderMenu/HeaderMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './HeaderMenu.css';
+
+const HeaderMenu = () => {
+  return (
+    <div className='header-menu'>
+        <ul>
+            <li>SALE</li>
+            <li>New</li>
+            <li>Best Sellers</li>
+            <li>Shop All</li>
+            <li>Discovery</li>
+        </ul>
+    </div>
+  )
+};
+
+export default HeaderMenu;
